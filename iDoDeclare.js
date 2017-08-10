82ed4356b39c64a7491c511ed699bc7e93d9e3ce
carlos arosemena <ceag68@hotmail.com>
2017-08-09 20:17:08 -0400

    IDoDeclare.js Dojo Assignment

---
+++ iDoDeclare.js
@@ -0,0 +1,28 @@
+var number1 = 900;
+var number2 = 2500;
+var number3 = -35.76;
+var number4 = 42;
+var string1 = "string 1"
+var string2 = "string 2"
+var string3 = "string 3"
+var bool1 = true;
+var bool2 = false;
+var und;
+console.log("============================")
+console.log("Let's start with the numbers: ")
+console.log("First Number: ",number1);
+console.log("Second Number: ",number2);
+console.log("Third Number: ",number3);
+console.log("Fourth Number: ",number4);
+console.log("============================")
+console.log("Now, it's time for some strings: ")
+console.log("First String: ", string1);
+console.log("Second String: ", string2);
+console.log("Third String: ", string3);
+console.log("============================")
+console.log("Moving on to booleans: ");
+console.log("Boolean true: ", bool1);
+console.log("Boolean false: ", bool2);
+console.log("============================");
+console.log("Closing with an undefined one: ");
+console.log("This variable is not assigned any value so it is: ", und);
