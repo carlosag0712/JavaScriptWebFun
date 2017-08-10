e9769e8ad02c359ab6cbde3dc84fc5cbfc447ca4
carlos arosemena <ceag68@hotmail.com>
2017-08-10 10:29:37 -0400

    While you wait assignment

---
+++ WhileYouWait.js
@@ -0,0 +1,21 @@
+var daysUntilMyBirthday = 60;
+
+
+while (daysUntilMyBirthday != 0) {
+
+  if(daysUntilMyBirthday >2){
+    console.log(daysUntilMyBirthday+ " days until my birthday. Such a long time... :(");
+  }
+  else if(daysUntilMyBirthday >=1 || daysUntilMyBirthday ==2) {
+    console.log(daysUntilMyBirthday+ " DAYS UNTIL MY BIRTHDAY!!!");
+
+  }
+
+
+  daysUntilMyBirthday--;
+
+}
+
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
+console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
