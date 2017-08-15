281f1f95ee17dfba50f91c08b163ca9e2dc9dc3b
carlos arosemena <ceag68@hotmail.com>
2017-08-14 21:49:41 -0400

    improved code thanks to Wes

--- StudentsAndInstructors.js
+++ StudentsAndInstructors.js
@@ -1,22 +1,25 @@
 
 // PART I:
 
-// var students = [
-//      {first_name:  'Michael', last_name : 'Jordan'},
-//      {first_name : 'John', last_name : 'Rosales'},
-//      {first_name : 'Mark', last_name : 'Guillen'},
-//      {first_name : 'KB', last_name : 'Tonel'}
-// ]
-//
-// function printArray(arr){
-//   for(var i=0; i<arr.length; i++){
-//     console.log(arr[i].first_name+" "+arr[i].last_name)
-//
-//   }
-// }
-//
-// printArray(students);
+var students = [
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+]
 
+function printArray(arr){
+  for(var i=0; i<arr.length; i++){
+    console.log(arr[i].first_name+" "+arr[i].last_name)
+
+  }
+}
+
+printArray(students);
+
+console.log("=================================================");
+console.log("PART II - OPTIONAL VERSION 1");
+console.log("=================================================");
 
 // Part II (Optional)
 
@@ -36,16 +39,43 @@ var users = {
 
 
 
+ function printArrayWithNumOfChars(data, property, property2){
+
+  //  for(key in data){
+  //    console.log(key);
+     for(var i=0;i<data[property].length;i++){
+
+       console.log((i+1)+" - "+data[property][i].first_name.toUpperCase()+" "+data[property][i].last_name.toUpperCase()+" - "+(data[property][i].first_name.length+data[property][i].last_name.length));
+     }
+
+     for(var i=0;i<data[property2].length;i++){
+       console.log((i+1)+" - "+data[property2][i].first_name.toUpperCase()+" "+data[property2][i].last_name.toUpperCase()+" - "+(data[property2][i].first_name.length+data[property2][i].last_name.length));
+     }
+
+  //  }
+
+ }
+
+ printArrayWithNumOfChars(users, "Students", "Instructors");
+
+
+ console.log("=================================================");
+ console.log("PART II - OPTIONAL VERSION 2");
+ console.log("=================================================");
+
  function printArrayWithNumOfChars(data){
 
    for(key in data){
      console.log(key);
      for(var i=0;i<data[key].length;i++){
-       console.log((i+1)+" "+data[key][i].first_name.toUpperCase()+" "+data[key][i].last_name.toUpperCase()+" - "+(data[key][i].first_name.length+data[key][i].last_name.length));
+
+       console.log((i+1)+" - "+data[key][i].first_name.toUpperCase()+" "+data[key][i].last_name.toUpperCase()+" - "+(data[key][i].first_name.length+data[key][i].last_name.length));
      }
+
+
 
    }
 
  }
 
- printArrayWithNumOfChars(users);
+ printArrayWithNumOfChars(users, "Students", "Instructors");
